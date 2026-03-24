3c242b1b3a6483d11442aadc5b21f7e90db57ed7
KoraEXE <korawitrolarieta@gmail.com>
2026-03-24 13:00:17 +0100

    v2

--- src/ventanas/SelecionJuego.java
+++ src/ventanas/SelecionJuego.java
@@ -27,48 +27,53 @@ public class SelecionJuego extends JDialog implements ActionListener {
 		contentPanel.setLayout(null);
 		{
 			JLabel lblNewLabel = new JLabel("");
-			lblNewLabel.setBounds(100, 25, 247, 70);
-			lblNewLabel.setIcon(new ImageIcon("C:\\Users\\1dami\\Downloads\\SelecionJuego.png"));
+			lblNewLabel.setBounds(140, 25, 154, 66);
+			lblNewLabel.setIcon(new ImageIcon("C:\\Users\\1dami\\Downloads\\SelecionJuego (1).png"));
 			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 			contentPanel.add(lblNewLabel);
 		}
 		{
 			JButton btnNewButton = new JButton("1. BLACK JACK");
-			btnNewButton.setBounds(150, 115, 148, 20);
+			btnNewButton.setBounds(140, 115, 154, 20);
 			contentPanel.add(btnNewButton);
 		}
 		{
 			JButton btnNewButton_1 = new JButton("2. STATS");
-			btnNewButton_1.setBounds(150, 140, 148, 20);
+			btnNewButton_1.setBounds(140, 140, 154, 20);
 			contentPanel.add(btnNewButton_1);
 		}
 		{
 			JButton btnNewButton_2 = new JButton("3. CERRAR SESIÓN");
-			btnNewButton_2.setBounds(150, 165, 148, 20);
+			btnNewButton_2.setBounds(140, 165, 154, 20);
 			contentPanel.add(btnNewButton_2);
 		}
 		{
 			JButton btnNewButton_3 = new JButton("4. BORRAR");
-			btnNewButton_3.setBounds(150, 190, 148, 20);
+			btnNewButton_3.setBounds(140, 190, 154, 20);
 			contentPanel.add(btnNewButton_3);
 		}
 		{
 			JButton btnNewButton_4 = new JButton("5. PRAY");
-			btnNewButton_4.setBounds(150, 215, 148, 20);
+			btnNewButton_4.setBounds(140, 215, 154, 20);
 			contentPanel.add(btnNewButton_4);
 		}
 		{
 			JLabel lblNewLabel_1 = new JLabel("");
 			lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\1dami\\Downloads\\Jimbo2 ORIGINAL.jpg"));
-			lblNewLabel_1.setBounds(20, 45, 120, 195);
+			lblNewLabel_1.setBounds(10, 40, 120, 195);
 			contentPanel.add(lblNewLabel_1);
 		}
 		{
 			JLabel lblNewLabel_1 = new JLabel("");
 			lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\1dami\\Downloads\\Jimbo1 ORIGINAL.jpg"));
-			lblNewLabel_1.setBounds(308, 45, 120, 195);
+			lblNewLabel_1.setBounds(306, 40, 120, 195);
 			contentPanel.add(lblNewLabel_1);
 		}
+		
+		JLabel Fondo = new JLabel("");
+		Fondo.setIcon(new ImageIcon("C:\\Users\\1dami\\Downloads\\fondo2 (1).png"));
+		Fondo.setBounds(0, 0, 436, 263);
+		contentPanel.add(Fondo);
 		
 	}
 
@@ -78,5 +83,4 @@ public class SelecionJuego extends JDialog implements ActionListener {
 		// TODO Auto-generated method stub
 
 	}
-
 }
